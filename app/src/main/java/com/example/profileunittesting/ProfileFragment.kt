ff6eb4e30b7ea7023656f7e1903dbce7b1901787
Perez Mac <apple@Timring.local>
2020-08-24 20:38:43 +0100

    complete profile layout

--- app/src/main/java/com/example/profileunittesting/ProfileFragment.kt
+++ app/src/main/java/com/example/profileunittesting/ProfileFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 
 
@@ -22,11 +21,24 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_profile, container, false)
 
+        //GET PROFILE FIELDS
+        val nameField = view.findViewById<TextView>(R.id.name_field)
+        val phoneField = view.findViewById<TextView>(R.id.phone_field)
+        val emailField = view.findViewById<TextView>(R.id.email_field)
+        val sexField = view.findViewById<TextView>(R.id.sex_field)
+
         //GET FORM INFO
-        val profile_text = view.findViewById<TextView>(R.id.profile_text)
         val bundle = this.arguments
-        val txt = bundle?.get("text")
-        profile_text.text = txt.toString()
+        val name = bundle?.get("name")
+        val phone = bundle?.get("phone")
+        val email = bundle?.get("email")
+        val sex = bundle?.get("sex")
+
+        //SET PROFILE FIELDS
+        nameField.text = name.toString()
+        phoneField.text = phone.toString()
+        emailField.text = email.toString()
+        sexField.text = sex.toString()
 
         return view
     }
